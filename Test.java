5ef1acee21178be29e4096e29346395a76a72c0f
sglbl <silo1881@gmail.com>
2021-11-23 15:58:40 +0100

    c3

--- Test.java
+++ Test.java
@@ -4,7 +4,7 @@ import javax.swing.*;
 
 public class Test {
     public static void main(String args[]){
-        Drawer board = new Drawer();
+        BoardCreator board = new BoardCreator();
 
         board.setDefaultCloseOperation( JFrame.EXIT_ON_CLOSE );
         board.setSize( 850, 600 ); // set frame size
